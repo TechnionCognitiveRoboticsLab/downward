78dfe9bd16f37fe5b47a9462691fb79b6581f596
Erez Karpas <karpase@gmail.com>
2023-06-04 14:20:27 +0300

    sampler and dataloader

--- src/search_dump_dataset.py
+++ src/search_dump_dataset.py
@@ -2,9 +2,11 @@ import pandas
 import numpy
 import sys
 import gzip
-from torch.utils.data import Dataset, ConcatDataset
+import torch
+from torch.utils.data import Dataset, ConcatDataset, RandomSampler, DataLoader, Sampler
 import random
 
+from speechbrain.dataio.sampler import ConcatDatasetBatchSampler
 
 class SingleFileSearchDumpDataset(Dataset):    
     def __init__(self, datafilename : str, 
@@ -95,117 +97,17 @@ class SearchDumpDataset(ConcatDataset):
             datasets.append(SingleFileSearchDumpDataset(filename, height, seq_len))
         ConcatDataset.__init__(self, datasets)
 
-# class SearchDumpDataset(Dataset):    
-#     def __init__(self, datafilename : str, 
-#                  height : int = 3, 
-#                  seq_len : int = 10,
-#                  min_expansions : int = 1000, 
-#                  max_expansions : int = 1000000, 
-#                  search_algorithm : str = "",
-#                  heuristic : str = "",
-#                  domain : str = "",
-#                  not_domain : bool = False):
-#         self.datafilename = datafilename
-#         self.height = height
-#         self.seq_len = seq_len
-#         self.min_expansions = min_expansions
-#         self.max_expansions = max_expansions
-#         self.search_algorithm = search_algorithm
-#         self.heuristic = heuristic
-#         self.domain = domain
-#         self.not_domain = not_domain
-        
-#         df = pandas.read_csv(datafilename)    
-#         df = df[(df.expansions <= self.max_expansions) & (df.expansions >= self.min_expansions)]
-#         if self.domain != "":
-#             if self.not_domain:
-#                 df = df[df.domain != self.domain]
-#             else:
-#                 df = df[df.domain == self.domain]
-#         if self.search_algorithm != "":
-#             df = df[df.search_algorithm == self.search_algorithm]
-#         if self.heuristic != "":
-#             df = df[df.heuristic == self.heuristic]
-#         self.data_df = df
-        
+class SearchDumpDatasetSampler(ConcatDatasetBatchSampler):
+    def __init__(self, ds : SearchDumpDataset, batch_size_per_dump : int = 1):        
+        ConcatDatasetBatchSampler.__init__(self,
+            [RandomSampler(dataset) for dataset in ds.datasets],
+            batch_sizes=[batch_size_per_dump for _ in ds.datasets]
+        )
 
-#         self.data = {}
 
-#         self.headers = []
-#         self.basic_header_names = []
-
-#         files_to_train = self.data_df.search_dump_file
-#         for filename in files_to_train:
-#             with gzip.open(filename, "r") as f:
-#                 dfr = pandas.read_csv(f, sep='\t')
-#                 df = dfr.set_index('id')
-#                 self.data[filename] = df
-
-#                 if len(self.headers) == 0:                    
-#                     self.headers = ['search_algorithm','heuristic','domain','problem', 'filename', 'search_progress']     
-#                     for c in dfr.columns:
-#                         if c != 'id' and c != 'path':
-#                             self.headers.append(c)
-#                             self.basic_header_names.append(c)
-#                     for i in range(self.height):
-#                         for c in dfr.columns:
-#                             if c != 'id' and c != 'path':
-#                                 self.headers.append('a' + str(i) + '_' + c)
-                
-
-#     def __len__(self):        
-#         return self.data_df.expansions.sum()
-    
-#     def get_search_dump(self, filename):
-#         return self.data[filename]
-    
-#     def generate_row_X(self, row, df):
-#         path = row.path
-#         crow = row[self.basic_header_names].to_numpy(dtype=numpy.float64)
-#         megarow_list = [#[data_row.search_algorithm, data_row.heuristic, data_row.domain, data_row.problem, data_row.search_dump_file],
-#                         crow]
-#         if isinstance(path, str):                    
-#             nodes = list(filter(lambda node: node != "", path.split(",")[::-1]))[:self.height]                    
-#         else:
-#             nodes = []
-#         for node in nodes:                    
-#             node_row = df.loc[node][self.basic_header_names].to_numpy(dtype=numpy.float64)
-#             if node_row.ndim > 1:
-#                 node_row = node_row[0,:].to_numpy(dtype=numpy.float64)
-#             megarow_list.append(node_row)                        
-#         megarow_list.append([0.0] * len(self.basic_header_names) * (self.height - len(nodes)))
-#         megarow = torch.as_tensor(numpy.concatenate(megarow_list,axis=None))
-#         return megarow
-
-#     def generate_row(self, data_row, relative_idx):
-#         df = self.get_search_dump(data_row.search_dump_file)
-#         row = df.iloc[relative_idx]
-#         label = torch.as_tensor( [row.N / (len(df.index) - 1)] )
-#         Xs = []
-#         current_index = relative_idx
-#         while current_index >= 0 and current_index > relative_idx - self.seq_len:
-#             row = df.iloc[current_index]
-#             Xs.append(self.generate_row_X(row, df))
-#             current_index = current_index - 1
-#         while current_index > relative_idx - self.seq_len:
-#             Xs.append(torch.as_tensor([0.0] * len(self.basic_header_names) * (self.height + 1)))
-#             current_index = current_index - 1
-#         return torch.stack(Xs), label
-    
-#     def __getitem__(self, idx):
-#         current_idx = 0
-#         for i, row in self.data_df.iterrows():
-#             if idx >= current_idx and idx < current_idx + row.expansions:
-#                 relative_idx = idx - current_idx
-#                 return self.generate_row(row, relative_idx)                                
-#             else:
-#                 current_idx = current_idx + row.expansions
-
-#         return None    
-
-import torch
 
 def main():
+    random.seed(42)
     filename="/home/karpase/git/downward/experiments/search_progress_estimate/data/search_progress_exp-eval/data.csv"    
     #filename=sys.argv[1]
     ds = SearchDumpDataset(filename, height=2, seq_len = 4, min_expansions=5, domain="depot", not_domain=True)
@@ -216,6 +118,15 @@ def main():
     print(ds[0])
     print(ds[1])
     print(ds[3])
+
+    sampler = SearchDumpDatasetSampler(ds)
+    dataloader = DataLoader(ds, batch_sampler=sampler)
+    
+    for batch in sampler:
+        print(batch)
+
+    for data_point in dataloader:
+        print(data_point)
 
     
 
